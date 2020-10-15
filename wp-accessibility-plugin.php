f2e620d99d9908bd1596e573ee61f72355ce939e
csaborio <christian@saborio.org>
2020-10-15 17:39:39 +1100

    composer file generated, base plugin file

---
+++ wp-accessibility-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package scorpiotek-accessibility-plugin
+ *
+ * @version 0.0.0.1
+ */
+/*
+Plugin Name: ScorpioTek Accessibility Plugin
+Plugin URI: https://github.com/csaborio001/wp-accessibility-toolbar.git
+Description: Enables the use of an accessibility toolbar for any website.
+Author: ScorpioTek
+Version: 0.0.0.1
+Author URI: https://www.scorpiotek.com
+*/
+
